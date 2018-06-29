5eb3cdfdbb02762bc59d24cca8b7bdd12c5bdd33
acenturyandabit <steeven.liu2@gmail.com>
2018-06-29 19:26:44 +1000

    added focusing

--- win/dwm.js
+++ win/dwm.js
@@ -1,12 +1,28 @@
+///GENERIC
+
+var idStor={};
+function gen_uid(){
+	do{
+		var rid=Math.floor(Math.random()*1000);
+	}while (idStor[rid]);
+	idStor[rid]=true;
+	return rid;
+}
+
 $(document).ready(()=>{
 	$("body").on("mouseup",moveEnd);
+	$("body").on("mousedown",defocus_all);
 	$("body").on("mousemove",moveCont);
-	// detect which OS we're on and show the respective toolbar.
 	
 	
 	
+	
+	
+	
+	///debugging
+	//newUser();
 });
-
+/////MOVING WINDOWS AROUND
 var movTarg;
 var moving=false;
 
@@ -27,23 +43,76 @@ function moveCont(e){
 function moveEnd(e){
 	moving=false;
 }
+//////////WINDOW FOCUS
+function defocus_all(){
+	$(".window").each((i,e)=>{
+		e.classList.remove("focused");
+	});
+	$(".barItem").each((i,e)=>{
+		e.classList.remove("focused");
+	});
+}
+
+function focusWnd(e){
+	
+	if (e.currentTarget){
+		e.stopImmediatePropagation();
+		e=e.currentTarget;
+	}
+	defocus_all();
+	e.classList.add("focused");
+	$("#i_"+e.id)[0].classList.add("focused");
+	
+}
+
+//////// taskbar
+function wndIconClick(e){
+	focusWnd($("#"+e.currentTarget.id.substring(2))[0]);
+	
+}
 
 
+////////
 
 
 function makeWindow(){
 	// Create the window div
 	newWnd=$("#proto>.window")[0].cloneNode(true);
+	newWnd.id=gen_uid();
 	$("#desktop").append(newWnd);
+	newWnd.children[0].addEventListener("mousedown",moveStart);
+	newWnd.addEventListener("mousedown",focusWnd);
+	newWnd.classList.add("focused");
 	
 	// Create the taskbar icon
+	newIco=$("#proto>.wnd_barItem")[0].cloneNode(true);
+	newIco.id="i_"+newWnd.id;
+	$("#winlist").append(newIco);
+	newIco.addEventListener("click",wndIconClick);
+	//newIco.addEventListener("click",focusWindow);
 	
 	
-	// Add handlers 
-	newWnd.children[0].addEventListener("mousedown",moveStart);
+	focusWnd(newWnd);
+	$("#startMenu").hide();
+}
+
+////USER STUFFS MANANAGEMENT
+function newUser(){//create an external user. not called when self is created.
+	
+	//create new user box
+	newUBox=$("#proto>.uBox")[0].cloneNode(true);
+	newUBox.id=gen_uid();
+	$("#desktop").append(newUBox);
+	
+	
+	newIco=$("#proto>.usr_barItem")[0].cloneNode(true);
+	newIco.id="i_"+newUBox.id;
+	$("#winlist").append(newIco);
+	
 	
 	
 }
+
 
 
 
@@ -53,3 +122,42 @@ function showStartMenu(){
 	
 	
 }
+
+
+
+//link it to a firebase backend, so you can collab edit a textbox
+
+
+//move viewport.
+
+//abstracted window content link? 
+
+
+
+/*
+	//make dragging the view work.
+	
+	
+Poll a file storage to get a list of files
+Open up a text file.
+A little taskbar for each window: save, run
+
+
+User shenanigans:
+remember user data position?
+
+// detect which OS we're on and show the respective toolbar.
+
+AIM: 
+- to be able to literally code this from a web browser, and code it collaboratively. 
+- Javascript/html/css as a first language. cos y not
+
+
+
+
+
+
+
+
+
+*/
